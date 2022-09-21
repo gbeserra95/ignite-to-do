0b156fce044215cc3cee3f23d10e14a147a5be3f
Gabriel <gabriel.fernandesb@gmail.com>
2022-09-21 17:37:45 -0300

    feat: header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { Header } from './components/Header';
+
 import './global.css';
+import styles from './App.module.css';
 
 export function App() {
-	return <h1>To do</h1>;
+	return (
+		<div>
+			<Header />
+			<main className={styles.wrapper}>
+				<h1>Hello world</h1>
+			</main>
+		</div>
+	);
 }
