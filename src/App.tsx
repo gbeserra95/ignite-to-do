ed9e924ff72a68b41f4524a77d993d6442ea9605
Gabriel <gabriel.fernandesb@gmail.com>
2022-09-21 18:25:40 -0300

    feat: counter components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Header } from './components/Header';
 
+import { PlusCircle } from 'phosphor-react';
+
 import './global.css';
 import styles from './App.module.css';
 
@@ -14,14 +16,17 @@ export function App() {
 						type='text'
 						placeholder='Adicione uma nova tarefa'
 					/>
-					<button type='submit'>Criar</button>
+					<button type='submit'>
+						Criar
+						<PlusCircle size={20} />
+					</button>
 				</form>
-				<div>
-					<div>
+				<div className={styles.infoWrapper}>
+					<div className={styles.createdTasks}>
 						<strong>Tarefas criadas</strong>
 						<span>5</span>
 					</div>
-					<div>
+					<div className={styles.completedTasks}>
 						<strong>Concluídas</strong>
 						<span>2 de 5</span>
 					</div>
