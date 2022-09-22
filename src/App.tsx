b5227749f8683dcb41d3bf54c9e02602b2246024
Gabriel <gabriel.fernandesb@gmail.com>
2022-09-22 15:01:34 -0300

    feat: delete function

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import { ChangeEvent, FormEvent, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
 import { Header } from './components/Header';
 import { EmptyList } from './components/EmptyList';
 import { ListItem } from './components/ListItem';
@@ -6,34 +9,61 @@ import { PlusCircle } from 'phosphor-react';
 
 import './global.css';
 import styles from './App.module.css';
-
-const tasks = [
-	{ completed: true, content: 'HTML' },
-	{ completed: true, content: 'CSS' },
-	{ completed: true, content: 'JavaScript' },
-	{ completed: true, content: 'React' },
-	{ completed: false, content: 'React Native' },
-	{ completed: false, content: 'Node' },
-];
-
-const isTasksEmpty = tasks.length === 0;
+export interface Task {
+	id: string;
+	completed: boolean;
+	content: string;
+}
 
 export function App() {
+	const [tasks, setTasks] = useState<Task[]>([]);
+	const [newTask, setNewTask] = useState('');
+
+	const isTasksEmpty = tasks.length === 0;
+	const isNewTaskEmpty = newTask.trim() === '';
+
+	function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+		setNewTask(event.target.value);
+	}
+
+	function handleSubmitForm(event: FormEvent) {
+		event.preventDefault();
+
+		setTasks([
+			...tasks,
+			{
+				id: uuidv4(),
+				completed: false,
+				content: newTask,
+			},
+		]);
+
+		setNewTask('');
+	}
+
+	function handleDeleteTask(id: string) {
+		const updatedTasks = tasks.filter(task => task.id !== id);
+		setTasks(updatedTasks);
+	}
+
 	return (
 		<div>
 			<Header />
 			<main className={styles.wrapper}>
-				<form className={styles.form}>
+				<form className={styles.form} onSubmit={handleSubmitForm}>
 					<input
 						className={styles.input}
 						type='text'
 						placeholder='Adicione uma nova tarefa'
+						onChange={handleInputChange}
+						value={newTask}
 					/>
-					<button type='submit'>
+					<button type='submit' disabled={isNewTaskEmpty}>
 						Criar
 						<PlusCircle size={20} />
 					</button>
 				</form>
+
 				<div className={styles.infoWrapper}>
 					<div className={styles.createdTasks}>
 						<strong>Tarefas criadas</strong>
@@ -44,12 +74,17 @@ export function App() {
 						<span>2 de 5</span>
 					</div>
 				</div>
+
 				{isTasksEmpty ? (
 					<EmptyList />
 				) : (
 					<ul className={styles.list}>
 						{tasks.map(task => (
-							<ListItem key={task.content} />
+							<ListItem
+								key={task.id}
+								task={task}
+								onDelete={handleDeleteTask}
+							/>
 						))}
 					</ul>
 				)}
--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,21 +1,29 @@
-import { useState } from 'react';
+import { Task } from '../App';
 import { Trash } from 'phosphor-react';
 
 import styles from './ListItem.module.css';
 
-export function ListItem() {
-	const [circleWeight, setCircleWeight] = useState<'duotone' | undefined>(
-		undefined
-	);
+interface ListItemProps {
+	task: Task;
+	onDelete: (id: string) => void;
+}
+
+export function ListItem({ task, onDelete }: ListItemProps) {
+	function handleOnDeleteTask() {
+		onDelete(task.id);
+	}
 
 	return (
 		<li className={styles.item}>
-			<input className={styles.checkbox} type='checkbox' />
-			<span>
-				Integer urna interdum massa libero auctor neque turpis turpis
-				semper. Duis vel sed fames integer.
+			<input
+				className={styles.checkbox}
+				checked={task.completed}
+				type='checkbox'
+			/>
+			<span className={task.completed ? styles.checked : undefined}>
+				{task.content}
 			</span>
-			<div className={styles.trash}>
+			<div className={styles.trash} onClick={handleOnDeleteTask}>
 				<Trash size={19} />
 			</div>
 		</li>
